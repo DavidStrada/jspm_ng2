73b21920a2f32fe801b3480f0431fb6b638030e7
David S <idavid.str@gmail.com>
2016-07-01 18:54:39 -0600

    initial commit

---
+++ jspm.config.js
@@ -0,0 +1,49 @@
+SystemJS.config({
+  paths: {
+    "github:": "jspm_packages/github/",
+    "npm:": "jspm_packages/npm/",
+    "app/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.16",
+      "os": "github:jspm/nodelibs-os@0.2.0-alpha"
+    },
+    "packages": {
+      "github:frankwallis/plugin-typescript@4.0.16": {
+        "map": {
+          "typescript": "npm:typescript@1.8.10"
+        }
+      },
+      "github:jspm/nodelibs-os@0.2.0-alpha": {
+        "map": {
+          "os-browserify": "npm:os-browserify@0.2.1"
+        }
+      }
+    }
+  },
+  transpiler: "plugin-typescript",
+  packages: {
+    "app": {
+      "main": "app.ts",
+      "meta": {
+        "*.ts": {
+          "loader": "plugin-typescript"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "github:*/*.json",
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
