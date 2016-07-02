f98c7e67ab5db1f14d36bb0f1703160a41a2d9de
David S <idavid.str@gmail.com>
2016-07-01 19:30:15 -0600

    initial boilerplate ng2 app

--- jspm.config.js
+++ jspm.config.js
@@ -29,13 +29,15 @@ SystemJS.config({
   packages: {
     "app": {
       "main": "app.ts",
+      defaultExtension: 'ts',
       "meta": {
         "*.ts": {
           "loader": "plugin-typescript"
         }
       }
     }
-  }
+  },
+  defaultJSxtension: false
 });
 
 SystemJS.config({
@@ -44,6 +46,17 @@ SystemJS.config({
     "npm:@*/*.json",
     "npm:*.json"
   ],
-  map: {},
+  map: {
+    "@angular/common": "npm:@angular/common@2.0.0-rc.4",
+    "@angular/compiler": "npm:@angular/compiler@2.0.0-rc.4",
+    "@angular/core": "npm:@angular/core@2.0.0-rc.4",
+    "@angular/platform-browser": "npm:@angular/platform-browser@2.0.0-rc.4",
+    "@angular/platform-browser-dynamic": "npm:@angular/platform-browser-dynamic@2.0.0-rc.4",
+    "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "reflect-metadata": "npm:reflect-metadata@0.1.2",
+    "rxjs": "npm:rxjs@5.0.0-beta.6",
+    "zone.js": "npm:zone.js@0.6.12"
+  },
   packages: {}
 });
